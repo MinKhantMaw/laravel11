342a847a53c3311c4224b014e0c14a2901004f9c
Min Khant Maw <minkhantmaw@onenex.co>
2024-05-02 06:39:57 +0630

    add: category and post migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Route::controller(PostController::class)->group(function () {
+//    Route::get('/', 'index')->name('posts.index');
+//    Route::post('/', 'store')->name('posts.store');
+//    Route::get('/{post}', 'show')->name('posts.show');
+//    Route::put('/{post}', 'update')->name('posts.update');
+//    Route::delete('/{post}', 'destroy')->name('posts.destroy');
+//});
+
+Route::resource('/posts', PostController::class);
